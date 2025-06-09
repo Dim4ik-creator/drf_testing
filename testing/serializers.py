b7bb863588c579b85a9a9d8a220f3fb480b3c6a3
Dim4ik-creator <141348723+Dim4ik-creator@users.noreply.github.com>
2025-06-09 21:24:08 +0300

    добавление сериализацией

--- testing/serializers.py
+++ testing/serializers.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 from .models import News
 
 
-class NewsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = News
-        fields = ("title","content")
+class NewsSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=50)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
--- testing/views.py
+++ testing/views.py
@@ -2,8 +2,25 @@ from rest_framework import generics
 from django.shortcuts import render
 from .models import News
 from .serializers import NewsSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.forms import model_to_dict
+
+# class NewsAPIView(generics.ListAPIView):
+#     queryset = News.objects.all()
+#     serializer_class = NewsSerializer
 
 
-class NewsAPIView(generics.ListAPIView):
-    queryset = News.objects.all()
-    serializer_class = NewsSerializer
+class NewsAPIView(APIView):
+    def get(self, request):
+        lst = News.objects.all()
+        return Response({"posts": NewsSerializer(lst, many=True).data})
+
+    def post(self, request):
+        serilizer = NewsSerializer(data=request.data)
+        serilizer.is_valid(raise_exception=True)
+        post_new = News.objects.create(
+            title=request.data["title"],
+            content=request.data["content"],
+        )
+        return Response({"post": NewsSerializer(post_new).data})
